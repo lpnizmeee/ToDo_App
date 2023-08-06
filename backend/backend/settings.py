fd7354deee9e7345f56a321fd50a2ade17f3b6d1
Lê Nam <lpnizmeee@lpnizmees-MacBook.local>
2023-08-06 17:26:00 +0700

    add cors_origin_whitelist fe_url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,9 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-)
+CORS_ORIGIN_WHITELIST = os.environ.get('FRONT_END_URL').split(' ')
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
